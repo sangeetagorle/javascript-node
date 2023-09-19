eb9c807704795e83649b194a97180f0bcc961e03
sangeetagorle <139069145+sangeetagorle@users.noreply.github.com>
2023-09-19 19:27:41 +0000

    background color changer

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,23 @@
-console.log("hello");
+const buttons=document.querySelectorAll('.button');
+const body=document.querySelector('body');
+
+buttons.forEach(function (button){
+    console.log(button);
+    button.addEventListener('click',function(e){
+    console.log(e);
+    console.log(e.target);
+    if(e.target.id==='gray'){
+        body.style.background=e.target.id;
+    }
+    if(e.target.id==='white'){
+        body.style.background=e.target.id;
+    }
+    if(e.target.id==='blue'){
+        body.style.background=e.target.id;
+    }
+    if(e.target.id==='yellow'){
+        body.style.background=e.target.id;
+    }
+    });
+});
+
